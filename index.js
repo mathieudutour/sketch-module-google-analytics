09f5d0a62d587b03743c21f1ad8525e6464d2348
Mathieu Dutour <mathieu@dutour.me>
2016-11-29 13:41:00 +0000

    :tada: first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var uuid = NSUserDefaults.standardUserDefaults().objectForKey(kUUIDKey)
+if (!uuid) {
+  uuid = NSUUID.UUID().UUIDString()
+  NSUserDefaults.standardUserDefaults().setObject_forKey(uuid, kUUIDKey)
+}
+
+function jsonToQueryString(json) {
+  return '?' + Object.keys(json).map(function(key) {
+    return encodeURIComponent(key) + '=' + encodeURIComponent(json[key]);
+  }).join('&')
+}
+
+module.exports = function (context, trackingId, hitType, props) {
+	var payload = {
+    v: 1,
+		tid: apiKey,
+		ds: 'Sketch%20' + NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString"),
+		cid: uuid,
+    t: hitType,
+    an: context.plugin.name,
+    aid: context.plugin.identifier,
+    av: context.plugin.version()
+	}
+	if (props) {
+		Object.keys(props).forEach(function (key) {
+      payload[key] = props[key]
+    })
+	}
+
+	var url = NSURL.URLWithString(
+    NSString.stringWithFormat("https://www.google-analytics.com/collect%@", jsonToQueryString(payload))
+  )
+
+	if (url) {
+    NSURLSession.sharedSession().dataTaskWithURL(url).resume()
+  }
+}
