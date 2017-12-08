fbccaf815483e42b5820f3e4db63a4d81de8245c
mathieudutour <mathieu@dutour.me>
2017-12-08 09:00:16 +0000

    version 0.1.4

--- index.js
+++ index.js
@@ -6,33 +6,33 @@ if (!uuid) {
 }
 
 function jsonToQueryString(json) {
-  return '?' + Object.keys(json).map(function(key) {
+  return '?' + Object.keys(json).map(function (key) {
     return encodeURIComponent(key) + '=' + encodeURIComponent(json[key]);
   }).join('&')
 }
 
 module.exports = function (context, trackingId, hitType, props) {
-	var payload = {
+  var payload = {
     v: 1,
-		tid: trackingId,
-		ds: 'Sketch ' + NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString"),
-		cid: uuid,
+    tid: trackingId,
+    ds: 'Sketch ' + NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString"),
+    cid: uuid,
     t: hitType,
     an: context.plugin.name(),
     aid: context.plugin.identifier(),
     av: context.plugin.version()
-	}
-	if (props) {
-		Object.keys(props).forEach(function (key) {
+  }
+  if (props) {
+    Object.keys(props).forEach(function (key) {
       payload[key] = props[key]
     })
-	}
+  }
 
-	var url = NSURL.URLWithString(
+  var url = NSURL.URLWithString(
     NSString.stringWithFormat("https://www.google-analytics.com/collect%@", jsonToQueryString(payload))
   )
 
-	if (url) {
+  if (url) {
     NSURLSession.sharedSession().dataTaskWithURL(url).resume()
   }
 }
